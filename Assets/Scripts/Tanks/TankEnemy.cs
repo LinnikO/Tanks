09ec9ddaebfaffaff4930aab8ba3df2204b9bdc8
LinnikOles <linnikoles@gmail.com>
2019-09-27 21:56:19 +0300

    Enemy tanks added

---
+++ Assets/Scripts/Tanks/TankEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankEnemy : Tank
+{
+    [SerializeField] int killScore;
+    [SerializeField] Tower tower;
+
+    public override void Fire()
+    {
+        tower.Fire();
+    }
+
+    public override void RotataTowerTo(Vector2 targetPoint)
+    {
+        tower.RotateToPoint(targetPoint);
+    }
+
+    protected override void DestroyTank()
+    {
+        EventManager.TriggerEvent(EventType.ENEMY_DESTROYED, killScore);
+        Destroy(this.gameObject);
+    }
+}
