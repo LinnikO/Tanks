5431e20027ed70ad4654ccf0e9283f522c423276
LinnikOles <linnikoles@gmail.com>
2019-09-27 23:01:40 +0300

    Projectiles added

---
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Projectile : MonoBehaviour
+{
+    public event Action Destroyed;
+
+    [SerializeField] Sprite playerProjectile;
+    [SerializeField] Sprite enemyProjectile;
+    [SerializeField] SpriteRenderer sprRenderer;
+
+    private ProjectileOwner owner;
+    private Vector2 direction;
+    private int damage;
+    private float speed;
+    private bool isMoving = false;
+
+    public ProjectileOwner Owner {
+        get { return owner; }
+        set {
+            owner = value;
+            if (owner == ProjectileOwner.PLAYER)
+            {
+                sprRenderer.sprite = playerProjectile;
+            }
+            else
+            {
+                sprRenderer.sprite = enemyProjectile;
+            }
+        }
+    }
+
+    public void LaunchProjectile(ProjectileOwner owner, Vector3 direction, int damage, float speed) {
+        Owner = owner;
+        this.direction = direction;
+        this.damage = damage;
+        this.speed = speed;
+        isMoving = true;
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            transform.position = (Vector2)transform.position + direction * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isMoving) {
+            return;
+        }
+
+        if ((Owner == ProjectileOwner.PLAYER && collision.transform.tag == "Enemy")
+            || (Owner == ProjectileOwner.ENEMY && collision.transform.tag == "Player"))
+        {
+            Tank tank = collision.gameObject.GetComponent<Tank>();
+            tank.TakeDamage(damage);
+            OnDestroyed();
+            Destroy(gameObject);
+        }
+        else if (collision.transform.tag == "Border")
+        {
+            OnDestroyed();
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroyed()
+    {
+        if (Destroyed != null) {
+            Destroyed();
+        }
+    }
+
+}
+
+public enum ProjectileOwner {
+    PLAYER,
+    ENEMY
+}
+
+public enum ProjectileType {
+    NORMAL,
+    SMALL
+}
---
+++ Assets/Scripts/Projectiles/ProjectileFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileFactory : MonoBehaviour
+{
+    [SerializeField] GameObject projectileNormal;
+    [SerializeField] GameObject projectileSmall;
+
+    public Projectile GetProjectile(ProjectileType type) {
+        switch (type)
+        {
+            case ProjectileType.NORMAL:
+                return Instantiate(projectileNormal).GetComponent<Projectile>();               
+            case ProjectileType.SMALL:
+                return Instantiate(projectileSmall).GetComponent<Projectile>();
+            default:
+                return null;
+        }
+    }
+}
