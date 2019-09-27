88eb10f47575204ed985da1a28ce347caa4fdf9f
LinnikOles <linnikoles@gmail.com>
2019-09-27 12:43:04 +0300

    GameController, EventManager added

---
+++ Assets/Scripts/Events/EventManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public static class EventManager
+{
+    private class MyObjectEvent : UnityEvent<object>
+    { }
+
+    private static Dictionary<EventType, UnityEvent<object>> eventListeners;
+
+    static EventManager() {
+        eventListeners = new Dictionary<EventType, UnityEvent<object>>();
+    }
+
+    public static void AddListener(EventType type, UnityAction<object> listener) {
+        UnityEvent<object> thisEvent;
+        if(eventListeners.TryGetValue(type, out thisEvent))    
+        {         
+            thisEvent.AddListener(listener);
+        }
+        else {
+            thisEvent = new MyObjectEvent();
+            thisEvent.AddListener(listener);
+            eventListeners.Add(type, thisEvent);
+        }       
+    }
+
+    public static void RemoveListener(EventType type, UnityAction<object> listener) {
+        UnityEvent<object> thisEvent;
+        if(eventListeners.TryGetValue(type, out thisEvent))      
+        {
+            thisEvent.RemoveListener(listener);            
+        }
+    }
+
+    public static void TriggerEvent(EventType type, object args) {
+        UnityEvent<object> thisEvent;
+        if (eventListeners.TryGetValue(type, out thisEvent))
+        {
+            thisEvent.Invoke(args);
+        }        
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void OnEnable()
+    {
+        EventManager.AddListener(EventType.START_GAME, StartGame);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.RemoveListener(EventType.START_GAME, StartGame);
+    }
+
+    private void StartGame(object args) {
+        SceneManager.LoadScene("Game");
+    }
+}
