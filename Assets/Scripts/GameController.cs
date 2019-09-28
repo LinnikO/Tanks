60be97a4ca0744999ba9a007ea462facd5c592af
LinnikOles <linnikoles@gmail.com>
2019-09-28 20:10:44 +0300

    GameData added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,22 +5,70 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] int attempts;
+
+    private GameData gameData;
+
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+        gameData = new GameData();
     }
 
     private void OnEnable()
     {
+        SceneManager.sceneLoaded += OnSceneLoaded;
         EventManager.AddListener(EventType.START_GAME, StartGame);
+        EventManager.AddListener(EventType.PLAYER_KILLED, OnPlayerKilled);
+        EventManager.AddListener(EventType.ENEMY_DESTROYED, OnEnemyDestroyed);
     }
 
     private void OnDisable()
     {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
         EventManager.RemoveListener(EventType.START_GAME, StartGame);
+        EventManager.RemoveListener(EventType.PLAYER_KILLED, OnPlayerKilled);
+        EventManager.RemoveListener(EventType.ENEMY_DESTROYED, OnEnemyDestroyed);
     }
 
     private void StartGame(object args) {
+        gameData.Attempts = 3;
+        gameData.Score = 0;
         SceneManager.LoadScene("Game");
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Game")
+        {
+            EventManager.TriggerEvent(EventType.SCORE_CHANGED, gameData.Score);
+            EventManager.TriggerEvent(EventType.ATTEMPS_CHANGED, gameData.Score);
+        }
+    }
+
+    private void OnPlayerKilled(object args) {
+        gameData.Attempts--;
+        EventManager.TriggerEvent(EventType.ATTEMPS_CHANGED, gameData.Score);
+        if (gameData.Attempts == 0)
+        {
+            GameOver();
+        }
+        else
+        {
+            EventManager.TriggerEvent(EventType.RESPAWN_PLAYER, null);
+        }
+    }
+
+    private void GameOver() {
+        object[] scoreData = new object[] { gameData.PreviousScore, gameData.Score };
+        EventManager.TriggerEvent(EventType.SHOW_GAME_OVER, scoreData);
+        gameData.PreviousScore = gameData.Score;
+    }
+
+    private void OnEnemyDestroyed(object args)
+    {
+        int score = (int)args;
+        gameData.Score += score;
+        EventManager.TriggerEvent(EventType.SCORE_CHANGED, gameData.Score);
     }
 }
---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameData
+{
+    public int PreviousScore {
+        get { return PlayerPrefs.GetInt("PreviousScore", 0); }
+        set { PlayerPrefs.SetInt("PreviousScore", value); }
+    }
+
+    public int Score { get; set; }
+
+    public int Attempts { get; set; }
+}
