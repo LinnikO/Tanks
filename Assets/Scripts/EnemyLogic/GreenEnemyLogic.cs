9d975223ac74a219e5d9572ac591246891e0f2c7
LinnikOles <linnikoles@gmail.com>
2019-09-30 12:00:32 +0300

    Green tank logic

---
+++ Assets/Scripts/EnemyLogic/GreenEnemyLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenEnemyLogic : EnemyLogicBase
+{
+    protected override void Update()
+    {
+        base.Update();
+        if (movePath == null || movePath.Count == 0)
+        {
+            FindMovePath(PlayerTransform.position);
+        }
+    }
+
+
+}
